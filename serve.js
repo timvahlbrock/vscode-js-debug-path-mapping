a951ae611571e41069148820cc5977a21fe1df4c
Tim Vahlbrock <tim@vahlbrock.de>
2022-04-19 15:52:23 +0200

    Initial commit

---
+++ serve.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    var filePath = './out/' + request.url;
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case ".map":
+            filePath = "non-existant-file";
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                response.writeHead(404);
+                response.end(content, 'utf-8');
+            } else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
---
+++ src/app.ts
@@ -0,0 +1,2 @@
+console.log("This is a test");
+console.log("The content of this file is not really important");
